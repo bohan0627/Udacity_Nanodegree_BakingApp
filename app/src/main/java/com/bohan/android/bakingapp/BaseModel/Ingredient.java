c97ae71c10fb7d8ce1f7595a8717e14950facbda
BoHan <han.bo1@husky.neu.edu>
2018-10-14 21:03:18 -0400

    First push to GitHub

---
+++ app/src/main/java/com/bohan/android/bakingapp/BaseModel/Ingredient.java
@@ -0,0 +1,9 @@
+package com.bohan.android.bakingapp.BaseModel;
+
+public abstract class Ingredient {
+    public abstract float quantity();
+    public abstract String measure();
+    public abstract String ingredient();
+
+
+}
