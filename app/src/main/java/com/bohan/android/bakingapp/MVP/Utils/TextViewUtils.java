e686f7a7e7a5e6633575f761af25d013a30b0dd6
BoHan <han.bo1@husky.neu.edu>
2018-10-27 20:36:55 -0400

    Change UI color and textSize

--- app/src/main/java/com/bohan/android/bakingapp/MVP/Utils/TextViewUtils.java
+++ app/src/main/java/com/bohan/android/bakingapp/MVP/Utils/TextViewUtils.java
@@ -4,8 +4,11 @@ package com.bohan.android.bakingapp.MVP.Utils;
  * Created by Bo Han.
  */
 
+import android.graphics.Color;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
 import android.widget.TextView;
 
@@ -16,6 +19,8 @@ public class TextViewUtils {
         int start = fullText.indexOf(styledText);
         int end = start + styledText.length();
         sb.setSpan(style, start, end, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        //This is for make title text size large
+        sb.setSpan(new RelativeSizeSpan(2.0f), start, end, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
         textView.setText(sb);
     }
 }
